b52417f5276463ee573587671bd2cfc4175e663c
yuzujoe <yuzun225@gmail.com>
2020-08-16 21:39:46 +0900

    alb listener rule

--- workspaces/networking/alb.tf
+++ workspaces/networking/alb.tf
@@ -3,10 +3,11 @@
 #####################
 
 resource "aws_lb" "ecs_example" {
-  name                       = "ecs-example"
-  load_balancer_type         = "application"
-  internal                   = false
-  idle_timeout               = 60
+  name               = "ecs-example"
+  load_balancer_type = "application"
+  internal           = false
+  idle_timeout       = 60
+  //  Set this place to false before deleting it.
   enable_deletion_protection = true
 
   subnets = [
@@ -72,6 +73,43 @@ resource "aws_lb_listener" "redirect_http_to_https" {
       protocol    = "HTTPS"
       status_code = "HTTP_301"
     }
+  }
+}
+
+resource "aws_lb_target_group" "ecs_example" {
+  name                 = "ecs-example"
+  target_type          = "ip"
+  vpc_id               = aws_vpc.ecs_vpc.id
+  port                 = 80
+  protocol             = "HTTP"
+  deregistration_delay = 300
+
+  health_check {
+    path                = "/"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = 200
+    port                = "traffic-port"
+    protocol            = "HTTP"
+  }
+
+  depends_on = [aws_lb.ecs_example]
+}
+
+resource "aws_lb_listener_rule" "ecs_example" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_example.arn
+  }
+
+  condition {
+    field  = "path-pattern"
+    values = ["/*"]
   }
 }
 
