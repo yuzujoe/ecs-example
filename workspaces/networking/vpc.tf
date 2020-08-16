94e952e7d124a5dc0c2327c18dc4fb56f4bd7d19
yuzujoe <yuzun225@gmail.com>
2020-08-16 14:57:48 +0900

    definition route table

--- workspaces/networking/vpc.tf
+++ workspaces/networking/vpc.tf
@@ -17,3 +17,30 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
   availability_zone       = "ap-northeast-1a"
 }
+
+resource "aws_internet_gateway" "ecs-example" {
+  vpc_id = aws_vpc.ecs_vpc.id
+
+  tags = {
+    Name = "ecs-example"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.ecs_vpc.id
+
+  tags = {
+    Name = "ecs-example"
+  }
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.ecs-example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
