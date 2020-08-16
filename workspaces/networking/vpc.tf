649297cd8b354c3bef48472ea046fc8ac5b0918e
yuzujoe <yuzun225@gmail.com>
2020-08-16 15:28:54 +0900

    create nat gateway

--- workspaces/networking/vpc.tf
+++ workspaces/networking/vpc.tf
@@ -80,3 +80,28 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private.id
   subnet_id      = aws_subnet.private.id
 }
+
+#####################
+# NAT Gateway
+#####################
+
+resource "aws_eip" "nat_gateway" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.ecs-example]
+}
+
+resource "aws_nat_gateway" "ecs-example" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id     = aws_subnet.public.id
+  depends_on    = [aws_internet_gateway.ecs-example]
+
+  tags = {
+    Name = "ecs-example"
+  }
+}
+
+resource "aws_route" "private" {
+  route_table_id         = aws_route_table.private.id
+  nat_gateway_id         = aws_nat_gateway.ecs-example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
