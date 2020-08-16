5d37703a5268c21e5cb13b47d7d7487bc321184c
yuzujoe <yuzun225@gmail.com>
2020-08-16 14:38:00 +0900

    create public subnets

--- workspaces/networking/vpc.tf
+++ workspaces/networking/vpc.tf
@@ -1,11 +1,19 @@
 resource "aws_vpc" "ecs_vpc" {
   cidr_block = var.cidr_block
-// Effectiveness of name resolution
+  // Effectiveness of name resolution
   enable_dns_support = true
-// Automatically assigning Public DNS
+  // Automatically assigning Public DNS
   enable_dns_hostnames = true
 
   tags = {
     Name = "ecs-example"
   }
+}
+
+resource "aws_subnet" "public" {
+  cidr_block = "10.9.0.0/24"
+  vpc_id     = aws_vpc.ecs_vpc.id
+  // Automatically assigning public ip address
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1a"
 }
