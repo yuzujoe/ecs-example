c5184f8b6f99feebffe346a0e40fe1e57bdb031e
yuzujoe <yuzun225@gmail.com>
2020-08-17 22:39:54 +0900

    create rds

---
+++ workspaces/computing/rds.tf
@@ -0,0 +1,73 @@
+resource "aws_db_parameter_group" "ecs_example" {
+  family = "mysql5.7"
+  name   = "ecs-example"
+
+  parameter {
+    name  = "character_set_database"
+    value = "uft8mb4"
+  }
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8mb4"
+  }
+}
+
+resource "aws_db_option_group" "ecs_example" {
+  engine_name          = "mysql"
+  major_engine_version = "5.7"
+  name                 = "ecs-example"
+
+  option {
+    option_name = "MARIADB_AUDIT_PLUGIN"
+  }
+}
+
+resource "aws_db_subnet_group" "ecs_example" {
+  name = "ecs-example"
+
+  subnet_ids = [
+    data.aws_subnet.private_0.id,
+    data.aws_subnet.private_1.id
+  ]
+}
+
+resource "aws_db_instance" "ecs_example" {
+  identifier                 = "ecs-example"
+  engine                     = "mysql"
+  engine_version             = "5.7.25"
+  instance_class             = "db.t3.small"
+  allocated_storage          = 20
+  max_allocated_storage      = 100
+  storage_type               = "gp2"
+  storage_encrypted          = true
+  kms_key_id                 = var.kms_key_arn
+  username                   = "admin"
+  password                   = "VeryStrongPassword!"
+  multi_az                   = true
+  publicly_accessible        = false
+  backup_window              = "09:10-09:40"
+  backup_retention_period    = 30
+  maintenance_window         = "mon:10:10-mon:10:40"
+  auto_minor_version_upgrade = false
+  deletion_protection        = true
+  skip_final_snapshot        = false
+  port                       = 3306
+  apply_immediately          = false
+  vpc_security_group_ids     = [module.mysql_sg.security_group_id]
+  parameter_group_name       = aws_db_parameter_group.ecs_example.name
+  option_group_name          = aws_db_option_group.ecs_example.name
+  db_subnet_group_name       = aws_db_subnet_group.ecs_example.name
+
+  lifecycle {
+    ignore_changes = [password]
+  }
+}
+
+module "mysql_sg" {
+  source      = "./security_group"
+  name        = "mysql-sg"
+  vpc_id      = data.aws_vpc.ecs_example.id
+  port        = 3306
+  cidr_blocks = [data.aws_vpc.ecs_example.cidr_block]
+}
---
+++ workspaces/computing/varable.tf
@@ -0,0 +1,3 @@
+variable "kms_key_arn" {
+  default = "arn:aws:kms:ap-northeast-1:273172227336:key/4c5f68b6-fc23-48a4-9a34-1cb34f99872b"
+}
