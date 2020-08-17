9bb9b44b08e9ce573ab527b9d9f6b517379426f0
yuzujoe <yuzun225@gmail.com>
2020-08-17 15:06:51 +0900

    create ecs-task role

--- workspaces/account/iam.tf
+++ workspaces/account/iam.tf
@@ -16,3 +16,35 @@ module "describe_regions_for_ec2" {
   identifier = "ec2.amazonaws.com"
   policy     = data.aws_iam_policy_document.allow_describe_regions.json
 }
+
+#####################
+# ECS Task Role
+#####################
+
+data "aws_iam_policy" "ecs_task_execution_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "ecs_task_execution" {
+  source_json = data.aws_iam_policy.ecs_task_execution_role_policy.policy
+  statement {
+    effect    = "Allow"
+    actions   = ["ssm:GetParameters", "kms:Decrypt"]
+    resources = ["*"]
+  }
+}
+
+module "ecs_task_execution_role" {
+  source     = "./iam_role"
+  name       = "ecs-task-execution"
+  identifier = "ecs-tasks.amazonaws.com"
+  policy     = data.aws_iam_policy_document.ecs_task_execution.json
+}
+
+#####################
+# OutPuts
+#####################
+
+output "ecs_task_execution_role" {
+  value = module.ecs_task_execution_role.iam_role_arn
+}
