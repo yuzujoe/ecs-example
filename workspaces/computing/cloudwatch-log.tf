11aa83db0b450bb066520d2fa03c6a99cb918ff8
yuzujoe <yuzun225@gmail.com>
2020-08-17 15:32:29 +0900

    create scheduled tasks cloudwatch log

--- workspaces/computing/cloudwatch-log.tf
+++ workspaces/computing/cloudwatch-log.tf
@@ -6,3 +6,8 @@ resource "aws_cloudwatch_log_group" "for_ecs" {
   name              = "/ecs/ecs-example"
   retention_in_days = 30
 }
+
+resource "aws_cloudwatch_log_group" "for_ecs_scheduled_tasks" {
+  name              = "/ecs-scheduled-tasks/ecs-example"
+  retention_in_days = 30
+}
