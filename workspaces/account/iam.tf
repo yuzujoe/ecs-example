35cba046ec58ad91a849dbee895df4c5e465d215
yuzujoe <yuzun225@gmail.com>
2020-08-21 10:31:50 +0900

    manage remote state accout s3

--- workspaces/account/iam.tf
+++ workspaces/account/iam.tf
@@ -55,15 +55,3 @@ module "ecs_event_role" {
   identifier = "events.amazonaws.com"
   policy     = data.aws_iam_policy.ecs_event_role_policy.policy
 }
-
-#####################
-# OutPuts
-#####################
-
-output "ecs_task_execution_role" {
-  value = module.ecs_task_execution_role.iam_role_arn
-}
-
-output "ecs_event_role" {
-  value = module.ecs_event_role.iam_role_arn
-}
